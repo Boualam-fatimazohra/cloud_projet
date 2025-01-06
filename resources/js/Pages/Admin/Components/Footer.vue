98657c1cae925c677e0ee092575bf622860b1da6
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-06 14:22:16 +0100

    git add .

---
+++ resources/js/Pages/Admin/Components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+      <!-- Contenu du pied de page -->
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer',
+    // Logique du composant
+  }
+  </script>
+  
+  <style scoped>
+  /* Styles CSS */
+  </style>
