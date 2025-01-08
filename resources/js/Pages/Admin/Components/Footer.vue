4a70e7dca0142a284f5e4183ef0b0e88090408ec
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-08 20:33:19 +0100

    Ajout du composant Footer.vue

--- resources/js/Pages/Admin/Components/Footer.vue
+++ resources/js/Pages/Admin/Components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
-    <footer>
-      <!-- Contenu du pied de page -->
-    </footer>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Footer',
-    // Logique du composant
-  }
-  </script>
-  
-  <style scoped>
-  /* Styles CSS */
-  </style>
+  <footer>
+    <p>Copyright © 2024 - Tous droits réservés.</p> 
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  // Logique du composant
+}
+</script>
+
+<style scoped>
+/* Styles CSS */
+</style>
