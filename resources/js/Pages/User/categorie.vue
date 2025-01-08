04a5cc6caaedcc2293d6d9669c39423d1e5ee606
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-08 21:01:15 +0100

    Updated categorie.vue with new form and submission handling

---
+++ resources/js/Pages/User/categorie.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <!-- Your HTML template here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Your script logic here
+  }
+  </script>
+  
+  <style>
+  /* Your styles here */
+  </style>
