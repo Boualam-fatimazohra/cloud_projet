ff0cd44a4f15e4a33a02dc1c5cb9a9c0f6301562
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-08 20:53:05 +0100

    Ajout de la configuration Ziggy et mise à jour de app.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path'; // Importez le module 'path' pour gérer les chemins
 
 export default defineConfig({
     plugins: [
@@ -16,8 +17,13 @@ export default defineConfig({
                 },
             },
         }),
-
-
-        
     ],
+    resolve: {
+        alias: {
+            // Ajoutez un alias pour Ziggy
+            'ziggy': path.resolve(__dirname, 'vendor/tightenco/ziggy/dist/vue.esm.js'),
+            // Optionnel : Ajoutez un alias pour simplifier les imports dans votre projet
+            '@': path.resolve(__dirname, 'resources/js'),
+        },
+    },
 });
