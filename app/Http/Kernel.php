35e0a6df66b744504e48d8edbb36ba6438b27213
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-11 17:08:12 +0100

    Forcer HTTPS et configurer Apache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\redirectAdmin;
+use App\Http\Middleware\RedirectAdmin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -70,6 +70,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'redirectAdmin' => redirectAdmin::class,
+        'redirectAdmin' => RedirectAdmin::class,
     ];
 }
