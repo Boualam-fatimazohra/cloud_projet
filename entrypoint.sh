4acac783b16f22fa7e737930c5e320a0d97a0a7f
Boualam Fatimazohra <boualamfatima97@gmail.com>
2025-01-12 10:09:43 +0100

    Forcer HTTPS et configurer Apache

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Attendre que la base de données soit prête (si nécessaire)
-# while ! nc -z $DB_HOST $DB_PORT; do
-#     sleep 1
-# done
-
 # Création du fichier .env si nécessaire
 if [ ! -f .env ]; then
     cp .env.example .env
@@ -26,15 +21,9 @@ php artisan config:clear
 php artisan route:clear
 php artisan cache:clear
 
-# Vérifier les permissions
-chown -R www-data:www-data /var/www/html/storage
-chmod -R 775 /var/www/html/storage
-chown -R www-data:www-data /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/bootstrap/cache
-
 # Migrations (avec --force pour l'environnement de production)
 php artisan migrate --force
-php artisan migrate:status
+
 # Vérifier si Apache est déjà en cours d'exécution
 if ! pgrep apache2; then
     echo "Redémarrage d'Apache..."
@@ -42,7 +31,6 @@ if ! pgrep apache2; then
 else
     echo "Apache est déjà en cours d'exécution."
 fi
-tail -f /var/log/apache2/error.log
-tail -f /var/log/apache2/error.log /var/www/html/storage/logs/laravel.log
+
 # Démarrage d'Apache en mode foreground
 exec apache2-foreground
